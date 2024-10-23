e47a7c1defc538d3a0f238d46951ac587ebd5cb0
lemmih <lemmih@gmail.com>
2024-10-23 16:28:56 +0200

    query the faucet address when computing the balance.

--- src/app.rs
+++ src/app.rs
@@ -83,14 +83,15 @@ pub fn Faucet() -> impl IntoView {
     let faucet_balance = create_local_resource_with_initial_value(
         move || rpc_context.get(),
         move |provider| async move {
-            provider
-                .wallet_balance(
-                    parse_address("t15ydyu3d65gznpp2qxwpkjsgz4waubeunn6upvla")
-                        .unwrap_or(Address::new_id(1)),
-                )
-                .await
-                .ok()
-                .unwrap_or(TokenAmount::from_atto(0))
+            if let Ok(LotusJson(addr)) = faucet_address().await {
+                provider
+                    .wallet_balance(addr)
+                    .await
+                    .ok()
+                    .unwrap_or(TokenAmount::from_atto(0))
+            } else {
+                TokenAmount::from_atto(0)
+            }
         },
         Some(TokenAmount::from_atto(0)),
     );
