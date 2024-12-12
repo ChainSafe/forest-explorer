10c8d40a66d6e1c38e1c728e27bea62202ccb389
David Ansermino <14164624+ansermino@users.noreply.github.com>
2024-12-12 15:53:47 -0500

    Add support to faucet for `0x` addresses (#67)
    Co-authored-by: Hubert <lesny.rumcajs@gmail.com>

--- src/utils.rs
+++ src/utils.rs
@@ -1,18 +1,6 @@
 use std::future::Future;
 
-use fvm_shared::address::{Address, Network};
 use leptos::{RwSignal, SignalUpdate as _};
-
-pub fn parse_address(s: &str) -> anyhow::Result<(Address, Network)> {
-    Ok(Network::Testnet
-        .parse_address(s)
-        .map(|addr| (addr, Network::Testnet))
-        .or_else(|_| {
-            Network::Mainnet
-                .parse_address(s)
-                .map(|addr| (addr, Network::Mainnet))
-        })?)
-}
 
 pub async fn catch_all(
     errors: RwSignal<Vec<String>>,
