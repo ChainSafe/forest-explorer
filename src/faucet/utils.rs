3291136700dfc057a27a32d0689132eb514b1b7d
Shashank <shashank@chainsafe.io>
2025-04-16 09:58:16 +0530

    more clear comment

--- src/faucet/utils.rs
+++ src/faucet/utils.rs
@@ -122,7 +122,7 @@ pub fn format_balance(balance: &TokenAmount, unit: &str) -> String {
     )
 }
 
-/// Formats a URL for faucet address history.
+/// Constructs a URL to lookup the faucet address history for a given address.
 pub fn format_address_url(base_url: &str, address: &str) -> String {
     format!("{}address/{}", base_url, address)
 }
